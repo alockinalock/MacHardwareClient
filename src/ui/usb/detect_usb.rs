6217580848b3b736843d593818e388e3e10a2e70
alockinalock <kanjigawa@gmail.com>
2024-03-30 18:23:46 -0500

    vendor, product, serial detection

--- src/ui/usb/detect_usb.rs
+++ src/ui/usb/detect_usb.rs
@@ -1,51 +1,54 @@
-use rusb::{Context, Device, DeviceDescriptor, DeviceList, Error, UsbContext};
+use rusb::{
+    Context, Device, DeviceDescriptor, DeviceHandle, DeviceList, Error, Language, PrimaryLanguage,
+    UsbContext,
+};
+use std::time::Duration;
 
-pub fn usb_detect_all(
-) -> Result<Vec<(DeviceDescriptor, String, u16, u16, String, Option<String>)>, rusb::Error> {
+pub fn usb_detect_all() -> Result<Vec<(u8, String, String, String)>, rusb::Error> {
     let context = Context::new()?;
 
     let device_list = context.devices()?;
     let mut devices_info = Vec::new();
 
     for device in device_list.iter() {
-        let device_info = get_device_info(&device)?;
+        let handle_device = device.open()?;
+        let device_info = get_device_info(&handle_device)?;
         devices_info.push(device_info);
     }
 
     Ok(devices_info)
 }
 
-fn get_device_info(
-    device: &Device<Context>,
-) -> Result<(DeviceDescriptor, String, u16, u16, String, Option<String>), rusb::Error> {
-    let device_desc = device.device_descriptor()?;
-    let product_id = device_desc.product_id();
-    let vendor_id = device_desc.vendor_id();
+fn get_device_info<T: UsbContext>(
+    handle: &DeviceHandle<T>,
+) -> Result<(u8, String, String, String), rusb::Error> {
+    let device_desc = handle.device().device_descriptor()?;
+    let timeout = Duration::from_secs(1);
+    let languages = handle.read_languages(timeout)?;
 
-    let serial_number_index = device_desc.serial_number_string_index();
-    let manufacturer_index = device_desc.manufacturer_string_index();
-    let product_index = device_desc.product_string_index();
+    let active_configurations = handle.active_configuration()?;
+    let manufacturer: String;
+    let product: String;
+    let serial_number: String;
 
-    /*
-    let serial_number = string_from_index(device, &serial_number_index);
-    let manufacturer = string_from_index(device, &manufacturer_index);
-    let product_name = string_from_index(device, &product_index);
-    */
+    if !languages.is_empty() {
+        let language = languages[0];
+        manufacturer = handle
+            .read_manufacturer_string(language, &device_desc, timeout)
+            .unwrap_or("Not found".to_string());
+        product = handle
+            .read_product_string(language, &device_desc, timeout)
+            .unwrap_or("Not found".to_string());
+        serial_number = handle
+            .read_serial_number_string(language, &device_desc, timeout)
+            .unwrap_or("Not found".to_string());
+    } else {
+        (manufacturer, product, serial_number) = (
+            "Unable to find".to_string(),
+            "Unable to find".to_string(),
+            "Unable to find".to_string(),
+        );
+    }
 
-    let product_name = "PLACEHOLDER".to_string();
-    let manufacturer = "PLACEHOLDER".to_string();
-    let serial_number = Some("PLACEHOLDER".to_string());
-
-    Ok((
-        device_desc,
-        manufacturer,
-        vendor_id,
-        product_id,
-        product_name,
-        serial_number,
-    ))
-}
-
-fn string_from_index(device: &Device<Context>, item_index: &Option<u8>) -> Result<String, Error> {
-    Ok("PLACEHOLDER".to_string())
+    Ok((active_configurations, manufacturer, product, serial_number))
 }
