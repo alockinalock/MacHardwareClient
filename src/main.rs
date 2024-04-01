096e046df1fc41325ec58f938a927151a6e9cad6
alockinalock <kanjigawa@gmail.com>
2024-03-31 22:28:51 -0500

    began work on gui

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use std::path::PathBuf;
+
+use relm4::prelude::*;
+
 pub mod ui;
 
 use ui::usb::*;
@@ -6,6 +10,8 @@ fn main() -> anyhow::Result<()> {
     // custom panic handler setup
     human_panic::setup_panic!(human_panic::metadata!());
 
+    // the function usb_detect_all() requires sudo permissions
+    // this is to let UsbHandler access to the usb ports
     if let Ok(devices) = detect_usb::usb_detect_all() {
         println!("Number: {}", devices.len());
 
